82dcac8037d9c172222e99493110712c29a829c7
Prasanta Dasgupta <prd@microsoft.com>
2021-03-18 20:50:18 +0530

    adding new fibo program

---
+++ fibo_new.py
@@ -0,0 +1,21 @@
+#Program to Print Fibonacci Series upto a said number of terms
+# Eg. Series for 8 No. of terms : 0 1 1 2 3 5 8 13
+
+def fibo(term):
+    if term == 0:
+        return 0
+    elif term == 1 or term == 2:
+        return 1
+    else:
+        return (fibo(term-2) + fibo(term - 1))
+
+print(" ************* Fibonacci Series ******************")
+
+nt = int(input("\n Please enter no. of terms for Series : "))
+
+print(f"\n Fibonacci Series upto {nt} Terms goes as : ", end="")
+
+for k in range(nt):
+    print(f"{fibo(k)}",end=", ")
+
+print("\n\n**************************************************")
